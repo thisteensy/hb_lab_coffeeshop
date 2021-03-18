e5bfb94d594b947e020c6f9ba07ce41e92c41f17
Tina Haiser <tinahaiser@gmail.com>
2021-03-18 03:34:04 +0000

    completed event handlers
    Co-authored-by: Seale Wong <seale.wong@gmail.com>

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -38,3 +38,16 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+
+$('.add-to-order').on('click', () => {
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+}); 
+
+
+
+$('#place-order').on('click', () => {
+  incrementCoffeeSold($('#cart-items').children().length);
+  
+  resetCart();
+});
